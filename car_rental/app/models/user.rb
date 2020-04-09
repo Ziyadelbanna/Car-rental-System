039446952d004994c0832b58183ce077b98963d8
Sara <you@example.com>
2020-04-09 08:48:18 +0200

    part 2 is finished

--- car_rental/app/models/user.rb
+++ car_rental/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :favourites
-  has_many :cars, through: :favourites, dependent: :destroy
+  has_many :cars, through: :favourites
   has_many :rentals, dependent: :destroy
   has_many :reviews
 
@@ -10,4 +10,16 @@ class User < ApplicationRecord
 						uniqueness: {case_sensitive: false}, length: {minimum: 5, maximum: 50},
 						format: {with: VALID_EMAIL_REGEX}
 	has_secure_password
+
+  validates :age, presence: true, numericality: {greater_than: 17 }
+  validates :name, presence: true, length: {maximum: 25, minimum: 3}
+  validates :address, presence: true, length: { minimum: 5}
+  validates :points, numericality: {greater_than: -1 }
+
+
+
+  def car_not_saved?(car_id)
+    # if count is < 1 means that the count is zero so it isn't saved
+    favourites.where(car_id: car_id).count < 1
+  end
 end
